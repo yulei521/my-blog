e3c531fc99e3fd71b12df6c51db7044088f3556e
zhangyulei3 <zhangyulei3@jd.com>
2022-04-18 21:31:32 +0800

    feat: 补充目录

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -10,19 +10,84 @@ export default defineUserConfig<DefaultThemeOptions>({
     logo: 'https://vuejs.org/images/logo.png',
     navbar: [
       {
-        text: '面试',
+        text: '文章总结',
+        link: '/summary/',
+      },
+      {
+        text: '技能点',
+        link: '/knowledge/',
+      },
+      {
+        text: '面试经历',
         link: '/interview/',
       },
-    ],
-    sidebar: [
       {
-        text: '国企&央企',
-        link: '/interview/',
-        children: [
-          '/interview/wukuang.md',
-          '/interview/yiliao.md',
-        ],
+        text: '读书笔记',
+        link: '/notes/',
+      },
+      {
+        text: '随笔',
+        link: '/informal/',
       }
     ],
+    sidebar: {
+      '/interview/': [
+        {
+          text: '国企&央企',
+          children: [
+            '/interview/README.md',
+            '/interview/wukuang.md',
+            '/interview/yiliao.md',
+          ],
+        },
+      ],
+      '/knowledge/': [
+        {
+          text: 'JavaScript',
+          children: [
+            '/knowledge/README.md',
+          ],
+        },
+        {
+          text: 'CSS',
+          children: [],
+        },
+        {
+          text: 'Vue',
+          children: [],
+        },
+        {
+          text: '浏览器',
+          children: [],
+        },
+        {
+          text: '打包&构建',
+          children: [],
+        },
+        {
+          text: 'React',
+          children: [],
+        },
+        {
+          text: '结构与算法',
+          children: [],
+        }
+      ]
+    },
   },
+  plugins: [
+    [
+      '@vuepress/plugin-search',
+      {
+        locales: {
+          '/': {
+            placeholder: 'Search',
+          },
+          '/zh/': {
+            placeholder: '搜索',
+          },
+        },
+      },
+    ],
+  ],
 })
